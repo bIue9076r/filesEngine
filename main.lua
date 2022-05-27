8317a34c5a649a0508394eebe3e54cf1b69048d3
blue <nobfi517@gmail.com>
2022-05-27 15:31:52 -0500

    main.lua changed

--- main.lua
+++ main.lua
@@ -1,15 +1,3 @@
-function listKeys(table,tpath)
-	tpath = tpath or 'ROOT'
-	for i,v in pairs(table) do
-		if type(v) == 'table' then
-			print(tpath..'.'..i..':')
-			listKeys(v,'\t'..tpath..'.'..i)
-		else
-			print(tpath..'.'..i)
-		end
-	end
-end
-
 function love.load()
 	require("/Engine/filesConfig")
 	listKeys(files)
