edd717aab720458c38f2fe70e541d5f57af037b2
blue <nobfi517@gmail.com>
2022-06-28 14:19:01 -0500

    Re-entry for mouse functions

--- Engine/Libaries/mouse.lua
+++ Engine/Libaries/mouse.lua
@@ -5,7 +5,9 @@ mouse.down1 = false
 mouse.down2 = false
 mouse.down3 = false
 mouse.loadedEvents = {}
+mouse.preloadedEvents = {}
 mouse.loadedEventsDraw = {}
+mouse.preloadedEventsDraw = {}
 
 function mouse.update()
 	mouse.x, mouse.y = love.mouse.getPosition()
@@ -16,11 +18,18 @@ function mouse.update()
 		if(mouse['down'..v.button]) then
 			inx = (v.buttonBounds.minx < mouse.x) and (v.buttonBounds.maxx > mouse.x)
 			iny = (v.buttonBounds.miny < mouse.y) and (v.buttonBounds.maxy > mouse.y)
-			if(inx) and (iny) then
-				v.eventFunction()
+			if (v.activated == true) then
 				mouse.loadedEvents[i] = nil
 			end
+			if(inx) and (iny) and (not(v.activated)) then
+				v.eventFunction()
+				v.activated = true
+			end
 		end
+	end
+	for i,v in pairs(mouse.preloadedEvents) do
+		table.insert(mouse.loadedEvents,v)
+		mouse.preloadedEvents[i] = nil
 	end
 end
 
@@ -29,11 +38,18 @@ function mouse.draw()
 		if(mouse['down'..v.button]) then
 			inx = (v.buttonBounds.minx < mouse.x) and (v.buttonBounds.maxx > mouse.x)
 			iny = (v.buttonBounds.miny < mouse.y) and (v.buttonBounds.maxy > mouse.y)
-			if(inx) and (iny) then
-				v.eventFunction()
+			if (v.activated == true) then
 				mouse.loadedEventsDraw[i] = nil
 			end
+			if(inx) and (iny) and (not(v.activated)) then
+				v.eventFunction()
+				v.activated = true
+			end
 		end
+	end
+	for i,v in pairs(mouse.preloadedEventsDraw) do
+		table.insert(mouse.loadedEventsDraw,v)
+		mouse.preloadedEventsDraw[i] = nil
 	end
 end
 
@@ -41,14 +57,14 @@ function mouse.Load(event,mseButton,evtBox)
 	if (mseButton > 3) and (mseButton < 1) then
 		mseButton = 1
 	end
-	table.insert(mouse.loadedEvents,{button = mseButton,eventFunction = event,buttonBounds = evtBox})
+	table.insert(mouse.preloadedEvents,{button = mseButton,eventFunction = event,buttonBounds = evtBox,activated = false})
 end
 
 function mouse.LoadDraw(event,mseButton,evtBox)
 	if (mseButton > 3) and (mseButton < 1) then
 		mseButton = 1
 	end
-	table.insert(mouse.loadedEventsDraw,{button = mseButton,eventFunction = event,buttonBounds = evtBox})
+	table.insert(mouse.preloadedEventsDraw,{button = mseButton,eventFunction = event,buttonBounds = evtBox,activated = false})
 end
 
 files.mouse = mouse
